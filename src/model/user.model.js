d5e14ab03ec6a32da13adb7a259bf305845cd766
Nilesh Raut <nileshraut898@gmail.com>
2025-02-27 23:39:55 +0530

    f

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -36,7 +36,6 @@ const userSchema=new mongoose.Schema({
 
 userSchema.pre("save",async function(next){
     if (!this.isModified("password")) return next();
-console.log(this.password);
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
