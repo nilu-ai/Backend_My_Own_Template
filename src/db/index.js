d49663e43c57300fc2955a9e0cf3d6373da0bea6
Nilesh Raut <nileshraut898@gmail.com>
2025-04-01 20:11:01 +0530

    Renamig

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose" 
 const dbconnect=async()=>{
     try {
-        const connectinstance=await mongoose.connect("mongodb://localhost:27017/ecommerse")
+        const connectinstance=await mongoose.connect(`${process.env.REACT_APP_MONGODB_URL}/${process.env.REACT_APP_DBNAME}`)
         console.log("You DB is Connect and up and Runnnig n host:" ,connectinstance.connection.host)
     } catch (error) {
         console.log("Some Error Occured",error);
